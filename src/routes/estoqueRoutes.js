c9bceb0dc4e12a7eefec26f92977c41ff1451096
eduardo praciano <edupraciano@gmail.com>
2025-02-03 00:22:22 -0300

    refact: padronização do layout das paginas | feat: inclusão do fiveicon

--- src/routes/estoqueRoutes.js
+++ src/routes/estoqueRoutes.js
@@ -1,5 +1,5 @@
-import express from "express";
-import estoqueController from "../controllers/estoqueController.js";
+import express from 'express';
+import estoqueController from '../controllers/estoqueController.js';
 
 const router = express.Router();
 
@@ -8,67 +8,56 @@ const router = express.Router();
   *********************************************************************************/
 
 // Rota para listar apenas os itens novos do estoque
-router.get("/itens-novos", estoqueController.getItensNovos);
+router.get('/itens-novos', estoqueController.getItensNovos);
 
 // Rota para renderizar a tabela apenas os itens novos do estoque
-router.get("/tabela/itens-novos", estoqueController.showItensNovos);
+router.get('/tabela/itens-novos', estoqueController.showItensNovos);
 
 // Rota para listar apenas os itens usados do estoque
-router.get("/itens-usados", estoqueController.getItensUsados);
+router.get('/itens-usados', estoqueController.getItensUsados);
 
 // Rota para renderizar a tabela apenas os itens usados do estoque
-router.get("/tabela/itens-usados", estoqueController.showItensUsados);
+router.get('/tabela/itens-usados', estoqueController.showItensUsados);
 
 // Rota para renderizar o formulário de entrada
-router.get("/entrada", estoqueController.renderEntradaForm);
+router.get('/entrada', estoqueController.renderEntradaForm);
 
 // Rota para inserir dados no estoque
-router.post("/entrada", estoqueController.create);
+router.post('/entrada', estoqueController.create);
 
 // rota para obter o último tombo
-router.get("/ultimo-tombo", estoqueController.fetchUltimoTombo);
+router.get('/ultimo-tombo', estoqueController.fetchUltimoTombo);
 
 // rota para mostrar tabela com estoque atual
-router.get("/tabela/estoqueatual", estoqueController.getAllEstoque);
+router.get('/tabela/estoqueatual', estoqueController.getAllEstoque);
 
 // Nova rota para visualizar item específico
 router.get('/visualizar/:id', estoqueController.visualizarItem);
 
 // Nova rota para mostrar quantidade única de itens no estoque
-router.get("/qtde-unica", estoqueController.getQtdeUnicaEstoque);
-
+router.get('/qtde-unica', estoqueController.getQtdeUnicaEstoque);
 
 router.delete('/excluir/:id', estoqueController.destroy);
-
-
-
-
-
 
 /* ********************************************************************************
                   Rotas para a SAÍDA de itens no Estoque
   *********************************************************************************/
 
 // Rota para renderizar o formulário de SAÍDA
-router.get("/saida", estoqueController.renderSaidaForm);
+router.get('/saida', estoqueController.renderSaidaForm);
 
 // rota para mostrar tabela com itens pagos
-router.get("/estoque/itenspagos", estoqueController.getAllItensPagos);
+router.get('/estoque/itenspagos', estoqueController.getAllItensPagos);
 
 // Rota para buscar itens disponíveis
-router.get("/api/itens-disponiveis", estoqueController.fetchItensDisponiveis);
+router.get('/api/itens-disponiveis', estoqueController.fetchItensDisponiveis);
 
 // Rota para registrar a saída de itens
-router.post("/saida", estoqueController.registrarSaida);
+router.post('/saida', estoqueController.registrarSaida);
 
 // Rota para visualizar um item pago específico
 router.get('/visualizar/:id', estoqueController.visualizarItemPago);
 
-
 router.get('/visualizar/itempago/:id', estoqueController.visualizarItemPago);
-
-
-
-
 
 export default router;
