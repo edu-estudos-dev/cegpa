ab8ef3728f7e75d2fdbca51484fa748bf4195706
unknown <edu.estudos.dev@gmail.com>
2025-07-14 10:13:27 -0300

    novos termos modificados, 185, 189 e 284

--- src/models/sequenciaModel.js
+++ src/models/sequenciaModel.js
@@ -1,11 +1,11 @@
 import connection from '../../db_config/connection.js';
 
 class SequenciaModel {
-   // Consulta a sequência atual sem incrementar
+   // Consulta a sequência atual sem incrementar   
    getSequenciaAtual = async (ano) => {
       try {
-         // Define o valor inicial como 284 para 2025, e 1 para outros anos
-         const initialValue = ano === 2025 ? 284 : 1;
+         // Define o valor inicial como 292 para 2025, e 1 para outros anos
+         const initialValue = ano === 2025 ? 292 : 1;
          const insertQuery = `
             INSERT INTO doc_sequencia (ano, sequencia)
             VALUES (?, ?)
